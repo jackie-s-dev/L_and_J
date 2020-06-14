4913340469c2c2adb8f3daea53f69362827f1771
jackie-s-dev <jackie_s_dev@yahoo.com>
2020-06-14 13:58:46 -0400

    Made a working wiki-parser to find the titles inside of a list.

--- jackies_notes/bs4_test.py
+++ jackies_notes/bs4_test.py
@@ -1,5 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+
+def remove_link_ends(string):
+    """
+    Parsing wikipedia tags often has embedded links.
+    This function will test if the given string is a
+    numbered link.
+    """
+    if (string[0] == '[' and string[len(string)-1] ==']'):
+        return False
+    return True
+
 """
 Example usage of python Beautiful Soup, which will substitute
 the already existing MediaWiki API since the API does not fit
@@ -16,16 +27,13 @@ results = soup.find(id='mw-content-text')
 
 # we will prioritize the first column
 headers = results.find_all('th')
-print(len(headers))
 list_elements = results.find_all('td')
 
 for i in range(0, len(list_elements), len(headers)):
     print(list_elements[i])
     print("================")
-"""
-how_many = int(len(list_elements)/len(headers))
-print(how_many)
-for i in range(0, how_many):
-    print(list_elements[i + how_many])
-    print("==========================")
-"""
+    href_attr = list_elements[i].find_all('a')
+    for attr in href_attr:
+        if (attr.string != None and remove_link_ends(attr.string)):
+            print(attr.string)
+    print("================")
